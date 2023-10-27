26d0fea7eeede909eecd8f6cd80d6f5648d4188d
Isisbr <109961274+Isisbr@users.noreply.github.com>
2023-10-27 08:46:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-// Pom ----------------------------------------
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador =0;
+let contador = 0;
 
-    while (contador < listaDeTeclas.length) {
+while(contador < listaDeTeclas.length) {
 
-        listaDeTeclas[contador].onclick = tocaSomPom;
-        contador = contador + 1;
-        console.log (contador);
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    listaDeTeclas[contador].classList[1];
+    console.log(instrumento);
+    const idAudio = '#som_${instrumento}'
+    tecla.onclick = function (){
+        tocaSom('#som_tecla_pom');
     }
+
+    contador = contador + 1;
+
+    console.log(contador);
+}
